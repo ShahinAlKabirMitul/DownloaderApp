c44b924cc345be373a115cca42c2635327fd3851
Shahin Al Kabir Mitul <mitul.cse@gmail.com>
2020-10-27 18:32:26 +0600

    Login View Complete

--- DownloaderApp/DownloaderApp.WPF/MainWindow.xaml.cs
+++ DownloaderApp/DownloaderApp.WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DownloaderViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,64 @@ namespace DownloaderApp.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        UserViewModel vm;
         public MainWindow()
         {
             InitializeComponent();
+             vm = new UserViewModel();
+            DataContext = vm;
+
+        }
+
+        
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //ImgShowHide.Source = new BitmapImage(new Uri(@"\image\show.png", UriKind.Relative));
+        }
+        public void ShowPassword()
+        {
+            ImgShowHide.Source = new BitmapImage(new Uri(@"\Images\hide.png", UriKind.Relative));
+            txtVisiblePasswordbox.Visibility = Visibility.Visible;
+            txtPasswordbox.Visibility = Visibility.Hidden;
+            txtVisiblePasswordbox.Text = txtPasswordbox.Password;
+        }
+
+        public void HidePassword()
+        {
+            ImgShowHide.Source = new BitmapImage(new Uri(@"\Images\show.png", UriKind.Relative));
+            txtVisiblePasswordbox.Visibility = Visibility.Hidden;
+            txtPasswordbox.Visibility = Visibility.Visible;
+            txtPasswordbox.Focus();
+        }
+
+        private void ImgShowHide_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            HidePassword();
+        }
+
+        private void ImgShowHide_MouseLeave(object sender, MouseEventArgs e)
+        {
+            HidePassword();
+        }
+
+        private void ImgShowHide_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ShowPassword();
+        }
+
+        private void txtPasswordbox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (txtPasswordbox.Password.Length > 0)
+            {
+                ImgShowHide.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                ImgShowHide.Visibility = Visibility.Hidden;
+            }
+            vm.txtPassword = txtPasswordbox.Password;
+            
         }
     }
 }
--- DownloaderApp/DownloaderViewModel/UserViewModel.cs
+++ DownloaderApp/DownloaderViewModel/UserViewModel.cs
@@ -1,29 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using System.Xml;
+using System.Xml.Serialization;
+
 using DownloaderModel;
 
 namespace DownloaderViewModel
 {
-   public class UserViewModel
-   {
+   public class UserViewModel: INotifyPropertyChanged
+    {
        private User _user;
-
-       public UserViewModel()
+        private List<User> _users;
+        private readonly DelegateCommand _loginCommand;
+        public ICommand LoginCommand => _loginCommand;
+       
+        public UserViewModel()
        {
            _user = new User();
-       }
+           _loginCommand = new DelegateCommand(OnLogIn);
 
-       public string txtUserName
+            _users = GetUsers();
+        }
+        public List<User> GetUsers()
+        {
+            XmlDataDocument xmldoc = new XmlDataDocument();
+            XmlNodeList xmlnode;
+            int i = 0;
+            string str = null;
+            FileStream fs = new FileStream("UserInfo.xml", FileMode.Open, FileAccess.Read);
+            xmldoc.Load(fs);
+            xmlnode = xmldoc.GetElementsByTagName("User");
+            List<User> users = new List<User>();
+
+
+            for (i = 0; i <= xmlnode.Count - 1; i++)
+            {
+               
+                users.Add(new User()
+                {
+                    UserName = xmlnode[i].ChildNodes.Item(0).InnerText.Trim(),
+                    Password = xmlnode[i].ChildNodes.Item(1).InnerText.Trim()
+                });
+
+
+            }
+            return users;
+        }
+        public string txtUserName
        {
            get { return _user.UserName; }
-           set { _user.UserName = value; }
+           set { _user.UserName = value; Changed("txtUserName"); }
        }
 
        public string txtPassword
        {
            get { return _user.Password; }
-           set { _user.Password = value; }
+           set { _user.Password = value;Changed("txtPassword"); }
        }
+
+        public bool CanLogIn(string userName,string passworld)
+        {
+           var isFound= _users.FirstOrDefault(s => s.UserName == userName && s.Password == passworld);
+            if (isFound == null)
+                return false;
+            return true;
+        }
+        public void OnLogIn(object commandParameter)
+        {
+            
+           var ddd = CanLogIn(txtUserName, txtPassword);
+            if (ddd)
+            {
+
+            }
+        }
+        
+        public void Changed(string Name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
